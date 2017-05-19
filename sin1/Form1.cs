d68d30915d46e0b60f50bb928cff5a0dc2938c0f
Telzhanov <askhat491@gmail.com>
2017-05-20 00:32:21 +0600

    Add project files.

---
+++ sin1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sin1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            float x = 0;
+                float y = 0;
+            while(true)
+            {
+                x = x + 1;
+                point point = new point(x, y);
+                point.Draw(e.Graphics);
+                Thread.Sleep(10);  
+            }
+        }
+    }
+}
---
+++ sin1/point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sin1
+{
+    class point
+    {
+        float x;
+        float y;
+        SolidBrush myBrush = new SolidBrush(Color.Red);
+        public point(float x,float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void Draw(Graphics g)
+        {
+            y =100*(float)Math.Cos(Math.PI * x / 180.0) +100;
+            g.FillEllipse(myBrush, new RectangleF(x,y, 5, 5));
+        }
+        
+    }
+}
